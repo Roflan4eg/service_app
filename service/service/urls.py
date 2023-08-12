636f2503c969230f02aa33f28696e1a36b21c228
iliagurov <iliagurov@ya.ru>
2023-08-13 03:41:10 +0400

    commit_1

---
+++ service/service/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from rest_framework import routers
+
+from services.views import SubscriptionView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+router = routers.DefaultRouter()
+router.register(r'api/subscriptions', SubscriptionView)
+
+urlpatterns += router.urls
