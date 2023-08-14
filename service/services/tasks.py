ddd93f48d4e3ab3f323718eb737782327ea84e4d
iliagurov <iliagurov@ya.ru>
2023-08-15 01:27:58 +0400

    commit_2

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -3,6 +3,8 @@ import time
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.conf import settings
+from django.core.cache import cache
 from django.db import transaction
 from django.db.models import F
 
@@ -13,12 +15,13 @@ def set_price(subscription_id):
 
     with transaction.atomic():
 
-        subscription = Subscription.objects.select_to_update().filter(id=subscription_id).annotate(
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
             annotated_price=F('service__full_price') -
                             F('service__full_price') * F('plan__discount_percent') / 100.00).first()
 
         subscription.price = subscription.annotated_price
         subscription.save()
+    cache.delete(settings.PRICE_CACHE_NAME)
 
 
 @shared_task(base=Singleton)
@@ -27,7 +30,8 @@ def set_comment(subscription_id):
 
     with transaction.atomic():
 
-        subscription = Subscription.objects.select_to_update().get(id=subscription_id)
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
 
         subscription.comment = str(datetime.datetime.now())
         subscription.save()
+    cache.delete(settings.PRICE_CACHE_NAME)
